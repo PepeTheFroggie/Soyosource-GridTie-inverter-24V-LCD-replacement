41ffbba1d9730d0e11d06f51826f6a4c072fca8c
PepeTheFroggie <37778140+PepeTheFroggie@users.noreply.github.com>
2023-02-20 22:44:39 +0100

    Add files via upload

---
+++ SoyoGraph_24v/WebStuff.ino
@@ -0,0 +1,209 @@
+
+void handleNotFound() 
+{
+  server.send(404, "text/plain", "Not Here");
+}
+
+void handleSettings() 
+{
+  String out;
+  char temp[400];
+  
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
+    if      (server.argName(i) == "rd")  IsRead = true;  
+    else if (server.argName(i) == "wb")  IsBat  = true;  
+    else if (server.argName(i) == "wp")  IsPow  = true;  
+    else if (server.argName(i) == "BatStartV") sd.startvolt = server.arg(i).toInt();
+    else if (server.argName(i) == "BatStopV")  sd.stopvolt  = server.arg(i).toInt();
+    else if (server.argName(i) == "BatPowerW") sd.bat_power = server.arg(i).toInt();
+    else if (server.argName(i) == "bat") batmode = true;  
+    else if (server.argName(i) == "pv")  batmode = false;  
+    else if (server.argName(i) == "lim") limit = true;  
+    else if (server.argName(i) == "nli") limit = false;  
+  }
+
+  out += "<body><center>";
+            
+  out += "<h1>Soyo Settings</h1>";         
+  out += "<br>\n";
+  
+  sprintf(temp,"devmodel %d <br>\n",sd.dev_model);
+  out += temp;
+  sprintf(temp,"devbatvolt %d <br>\n",sd.dev_type);
+  out += temp;
+  if (batmode) out += "Bat mode<br>\n";
+  else         out += "PV mode<br>\n";
+  if (limit) out += "limit<br>\n";
+  else       out += "no limit<br>\n";
+  out += "<br>\n";
+    
+  out += "<form action=\"/settings\">\n";
+  out += "Bat start Volt : ";
+  sprintf(temp,"<input type=\"number\" name=\"BatStartV\" value=%d><br>\n",sd.startvolt);
+  out += temp;
+  out += "Bat stop Volt : ";
+  sprintf(temp,"<input type=\"number\" name=\"BatStopV\" value=%d><br>\n",sd.stopvolt);
+  out += temp;
+  out += "Bat Power W: ";
+  sprintf(temp,"<input type=\"number\" name=\"BatPowerW\" value=%d><br>\n",sd.bat_power);
+  out += temp;
+  out += "<input type=\"submit\" value=\"Submit\"><br>\n";
+  out += "</form>\n";
+  sprintf(temp,"startdelay %d <br>\n",sd.delaysec);
+  out += temp;
+  out += "<br>\n";
+
+  out += "<button onclick=\"window.location.href='/settings?rd=1\';\">Read Values</button>\n";
+  out += "<br><br>\n";
+  out += "<button onclick=\"window.location.href='/settings?wb=1\';\">Write Bat V</button>\n";
+  out += "<br><br>\n";
+  out += "<button onclick=\"window.location.href='/settings?wp=1\';\">Write Power</button>\n";
+  out += "<br><br>\n";
+  out += "<a href='/settings?bat=1'>batmode</a>";
+  out += "&nbsp\n";
+  out += "<a href='/settings?pv=1'>pvmode</a>";
+  out += "&nbsp\n";
+  out += "<a href='/settings?nli=1'>nolimit</a>";
+  out += "&nbsp\n";
+  out += "<a href='/settings?lim=1'>limit</a>";
+  out += "<br><br>\n";
+  out += "<a href='/'>Go Back</a>";
+  
+  out += "</center></body></html>";
+  server.send(200, "text/html", out);
+}
+
+void getData() 
+{
+  String out;
+  char temp[400];
+
+  out = "<html>\
+         <head>\
+           <meta http-equiv='refresh' content='20'/>\
+           <title>SoyoPower</title>\
+         </head>";
+  out += "<body>\
+          <center>\
+            <h1>Soyo Power Monitor</h1>";
+
+  sprintf(temp,"msg %d &nbsp\n",msgct);
+  out += temp;
+  sprintf(temp,"errmsg %d &nbsp\n",errmsgct);
+  out += temp;
+  sprintf(temp,"cserr %d <br>\n",cserr);
+  out += temp;
+  sprintf(temp,"power %d W<br><br>\n",(int)(sd.V_input*sd.A_input));
+  out += temp;
+  sprintf(temp,"opmode %x <br>\n",sd.opmode>>4);
+  out += temp;
+  sprintf(temp,"frame %x <br>\n",sd.opmode&0x0F);
+  out += temp;
+  sprintf(temp,"status %x <br>\n",sd.errstat);
+  out += temp;
+  sprintf(temp,"V input %.1f <br>\n",sd.V_input);
+  out += temp;
+  sprintf(temp,"A input %.1f <br>\n",sd.A_input);
+  out += temp;
+  sprintf(temp,"V main %.1f <br>\n",sd.V_main);
+  out += temp;
+  sprintf(temp,"HZ main %.1f <br>\n",sd.net_HZ);
+  out += temp;
+  sprintf(temp,"temp %.2f <br>\n",sd.temp);
+  out += temp;
+
+  out += "<br>\n";
+  out += "<button onclick=\"window.location.href='/settings?rd=1\';\">Settings</button>\n";
+  out += "<br><br>\n";
+  out += "<a href='/pwr.svg'>Graph</a>";
+  out += "<br><br>\n";
+  out += "<a href='/up'>Upload</a>";
+  out += "<br><br>\n";
+  
+  sprintf(temp,"%02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x<br>\n",se[1],se[2],se[3],se[4],se[5],se[6],se[7],se[8],se[9],se[10],se[11],se[12],se[12]); 
+  out += temp;
+
+  out += "</center></body></html>";
+  
+  server.send(200, "text/html", out);
+}
+
+#define num_datasets 720 // every minute, 12h 
+#define num_sensors_max 6
+#define num_sensors 3 
+String colors[num_sensors_max] = {"red","green","magenta","orange","lime","yellow"};
+
+struct wp
+{
+  uint8_t val[num_sensors];
+  int8_t hour;
+};
+
+wp wp_arr[num_datasets];
+uint16_t datasetpt = 0;
+
+void clearwp(){ for(int i=0;i<num_datasets;i++) wp_arr[i].hour = -1; }
+
+void storewp(float V, float A, int8_t hour)
+{
+  wp_arr[datasetpt].hour = hour;
+  wp_arr[datasetpt].val[0] = (uint8_t)(5.0*V); // 0..50V
+  wp_arr[datasetpt].val[1] = (uint8_t)(5.0*A); // 0..50A
+  wp_arr[datasetpt].val[2] = (uint8_t)(0.25*A*V); // 0..1000w
+  datasetpt++;
+  if (datasetpt >= num_datasets) datasetpt = 0;
+}
+
+void drawGraph() 
+{
+  String out;
+  out.reserve(3800);
+  int8_t oldhour = -1;
+  char temp[200];
+
+  out += "<svg viewBox=\"0 0 740 240\" xmlns=\"http://www.w3.org/2000/svg\">\n";
+  out += "<rect x=\"10\" y=\"0\" width=\"720\" height=\"240\" stroke=\"black\" fill=\"none\"/>\n";
+      
+  for (int i=0;i<num_datasets;i++)
+  {
+    int8_t newhour;
+    uint16_t pos = datasetpt + i;
+    if (pos >= num_datasets) pos -= num_datasets;
+    newhour = wp_arr[pos].hour;
+    if ((newhour != oldhour) && (oldhour >= 0))
+    {
+      if ((newhour == 12) || (newhour == 0))  // 12, 24
+        sprintf(temp,"<line x1=\"%d\" y1=\"240\" x2=\"%d\" y2=\"210\" stroke=\"black\" />\n",10+i,10+i);
+      else if ((newhour == 6) || (newhour == 18)) // 6, 18
+        sprintf(temp,"<line x1=\"%d\" y1=\"240\" x2=\"%d\" y2=\"220\" stroke=\"black\" />\n",10+i,10+i);
+      else 
+        sprintf(temp,"<line x1=\"%d\" y1=\"240\" x2=\"%d\" y2=\"230\" stroke=\"black\" />\n",10+i,10+i);
+      out += temp;        
+    }
+    oldhour = newhour;
+  }
+
+  for (int k=0;k<num_sensors;k++)
+  {
+    out += "<polyline points=\"";
+    // datasetpt is now, show the past
+    for (int i=0;i<num_datasets;i++)
+    {
+      uint16_t pos = datasetpt + i;
+      if (pos >= num_datasets) pos -= num_datasets;
+      if (wp_arr[pos].hour >= 0)
+      {
+        int tempdata = wp_arr[pos].val[k];
+        sprintf(temp,"%d,%d ",10+i,240-tempdata);
+        out += temp;
+      }
+    }
+    out += "\" fill=\"none\" stroke=\""; out += colors[k]; out += "\" />\n"; 
+  }
+  
+  //strsize = out.length();  
+  out += "</svg>\n";
+  
+  server.send(200, "image/svg+xml", out);
+}
---
+++ SoyoGraph_24v/soyo.ino
@@ -0,0 +1,142 @@
+// soyo -> esp direction
+//----------------------
+// A6 01 01 21 00 00 E3 00 3B 00 D7 64 01 D3 AF
+// sy          op vv vv aa aa VV VV hz tt tt cc
+
+// status data
+// Byte Len  Payload                Content          
+// 0     1   0xA6                   Header
+// 1&2   2   0x00 0x84              Output Power         
+// 3     1   0x91                   Operation mode (High nib), Frame function (Low nib)
+// 4     1   0x40                   Error and status bitmask
+// 5+6   2   0x01 0xC5              Battery voltage
+// 7+6   2   0x00 0xDB              Battery current
+// 9+10  2   0x00 0xF7              Grid voltage
+// 11    1   0x63                   Grid frequency
+// 12+13 2   0x02 0xBC              Temperature
+
+// settings data
+// Byte Len  Payload                Content       
+// 0     1   0xA6                   Header
+// 1     1   0x00                   Unknown
+// 2     1   0x72
+// 3     1   0x93                   Operation mode (High nib), Frame function (Low nib)
+// 4     1   0x40                   Operation status bitmask
+// 5     1   0xD4                   Device model
+// 6     1   0x30                   Device type
+// 7     1   0x2C                   Starting voltage  
+// 8     1   0x2B                   Shutdown voltage
+// 9     2   0x00 0xFA              Grid voltage  
+// 11    1   0x64                   Grid frequency
+// 12    1   0x5A                   Battery output power in 10W
+// 13    1   0x03                   Delay in seconds
+
+// read settings response: 53  10 B2 53 2 D4 30 2E 2C 0 DF 64 A 8
+
+void copy_msg_soyo_esp()
+{
+  byte msgty = se[3] & 0x0F;
+  if (msgty == 0x01)
+  {
+    sd.reqpower = (int)((se[1]<<8)+se[2]);
+    sd.opmode   = se[3];
+// high nibble  Limiter bit, Standby bit, PV mode bit, Battery mode bit
+    sd.errstat  = se[4];
+// 2=normal, 1=startip, 0=idle, 42=liniter+normal?
+    sd.V_input  = 0.1 * (float)((se[5]<<8)+se[6]);
+    sd.A_input  = 0.1 * (float)((se[7]<<8)+se[8]);
+    sd.V_main   =       (float)((se[9]<<8)+se[10]);
+    sd.net_HZ   = 0.5 * (float)se[11];
+    sd.temp     = 0.1 * (float)((se[12]<<8)+se[13]) - 30.0;
+  }
+  else if (msgty == 0x03)
+  {
+    sd.dev_model = se[5]; //0xD4 = 212 -> 220V, 112 -> 110V, 210 -> 1000W
+    sd.dev_type  = se[6]; //built for bat volt
+    sd.startvolt = se[7];
+    sd.stopvolt  = se[8];
+    sd.bat_power = 10 * se[12];
+    sd.delaysec  = se[13];   
+  }
+  else if (msgty == 0x00) // old 24V soyo
+  {
+    // 64 B7 60 2 0 0 0 0 0 DC 64 1 EB
+    sd.reqpower = (int)((se[1]<<8)+se[2]);
+    sd.opmode   = se[3];
+// high nibble  Limiter bit, Standby bit, PV mode bit, Battery mode bit
+    sd.errstat  = se[4];
+    sd.V_input  = 0.1 * (float)((se[5]<<8)+se[6]);
+    sd.A_input  = 0.1 * (float)((se[7]<<8)+se[8]);
+    sd.V_main   =       (float)((se[9]<<8)+se[10]);
+    sd.net_HZ   = 0.5 * (float)se[11];
+    sd.temp     = 0.1 * (float)((se[12]<<8)+se[13]) - 30.0;  
+  }
+  else errmsgct++;
+}
+
+byte chksum_se(byte * data)
+{
+  byte cs = 0;
+  for (int i=1;i<=13;i++) cs+=data[i];
+  return 0xFF - cs;
+}
+
+// esp -> soyo direction
+//----------------------
+// idle msg 
+// 55 01 00 00 00 FE
+// sy id b1 b3
+// sy id bp
+// sy id xx xx bs
+// b1 startvolt in 0.1V
+// b2 stopvolt = 0.1V
+// bp batpower in 10W
+// bs 2=bat pow, 1= pv
+// id 01=norm, 02 modeset, 0A=batset, 12=powerset
+
+byte chksum_es(byte * data)
+{
+  byte cs = 0;
+  for (int i=1;i<=4;i++) cs+=data[i];
+  return 0xFF - cs;
+}
+
+//55 01 00 00 00 FE idle msg back
+//55 02 02 00 02 F9 battery set
+//55 02 02 00 01 FC battery not set
+//55 0A FE 00 00 F7 battery start volt 25.4 
+//55 0A FD 03 00 F4 battery end volt 3 
+//55 12 02 00 00 EB battery power 20W
+
+void msg_esp_soyo(byte com)
+{
+  es[0]  = 0x55; // sync
+  es[1]  = com;
+  if (com == 0x01) //norm
+  {
+    es[2]  = 0x00;
+    es[3]  = 0x00;
+    es[4]  = 0x00;
+  }
+  else if (com == 0x02) // mode
+  {
+    es[2]  = 0;
+    es[3]  = 0;
+    if (batmode) es[4] = 2; // 01=PV 02=Bat
+    else         es[4] = 1;
+    if (limit)   es[4] += 0x10;
+  }
+  else if (com == 0x0A) // bat
+  {
+    es[2]  = sd.startvolt;
+    es[3]  = sd.stopvolt;
+    es[4]  = 0;
+  }
+  else if (com == 0x12) // pow
+  {
+    es[2]  = sd.bat_power/10;
+    es[3]  = 0;
+    es[4]  = 0;
+  }
+  es[5] = chksum_es((byte*)&es);
+}
